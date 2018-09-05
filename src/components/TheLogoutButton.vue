9915e538bd9a9a7b132741b3b1675acf1b9c2d45
m.navarro <m.navarro@payvision.com>
2018-09-05 19:25:07 +0200

    New logout component

---
+++ src/components/TheLogoutButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-menu 
+    bottom 
+    left>
+    <v-avatar 
+      slot="activator"
+      color="teal" 
+      size="32">
+      <span class="white--text">{{ user.displayName.charAt(0).toUpperCase() }}</span>
+    </v-avatar>
+
+    <v-list light>
+      <v-list-tile
+        @click="logout"
+      >
+        <v-list-tile-title 
+          v-text="'Salir'"/>
+        <v-list-tile-action>
+          <v-icon 
+            right 
+            class="ml-3" >logout</v-icon>
+        </v-list-tile-action>
+      </v-list-tile>
+    </v-list>
+  </v-menu>
+
+
+</template>
+<script>
+export default {
+  name: 'TheLogoutButton',
+  computed: {
+    auth() {
+      return this.$store.state.auth
+    },
+    user() {
+      return this.auth.user || {}
+    },
+  },
+  methods: {
+    logout() {
+      this.$store.dispatch('logout')
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.v-list__tile__action {
+  min-width: auto;
+}
+</style>
