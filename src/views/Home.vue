372fcf727c67001a354e5c9dd124722f50950438
m.navarro <m.navarro@payvision.com>
2018-09-08 14:50:10 +0200

    Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,24 +2,61 @@
   <v-container >
     <v-slide-y-transition mode="out-in">
       <v-layout 
-        row 
-        align-center>
-        <section>
-          <h3 class="font-weight-regular home-title mb-2">Últimos cambios en listas</h3>
-          <h4 class="font-weight-regular home-subheading mb-4">Mantente informado de los cambios en todas las listas</h4>
-          <stat-home-card :data="data.normalList" />
-        </section>
+        column 
+        align-let>
+        <article v-if="data.assignmentList">
+          <h3 class="font-weight-regular home-title mb-2">Lista de asignaciones</h3>
+          <h4 class="font-weight-regular home-subheading mb-4">Número de personas de la última asginación</h4>
+          <stat-dynamic-home-card
+            :data="data.assignmentList" 
+            :list="'assignmentList'"
+            class="mb-5"/>
+        </article>
+        <article v-if="data.citationList">
+          <h3 class="font-weight-regular home-title mb-2">Lista de citaciones</h3>
+          <h4 class="font-weight-regular home-subheading mb-4">Número de personas de la última citación</h4>
+          <stat-dynamic-home-card
+            :data="data.citationList" 
+            :list="'citationList'"
+            class="mb-5"/>
+        </article>
+        <article v-if="data.normalList">
+          <h3 class="font-weight-regular home-title mb-2">Lista normal</h3>
+          <h4 class="font-weight-regular home-subheading mb-4">Número de personas en el listado normal</h4>
+          <stat-static-home-card 
+            :data="data.normalList" 
+            :list="'normalList'"
+            class="mb-5"/>
+        </article>
+        <article v-if="data.bilingualList">
+          <h3 class="font-weight-regular home-title mb-2">Lista bilingüe</h3>
+          <h4 class="font-weight-regular home-subheading mb-4">Número de personas en el listado bilingüe</h4>
+          <stat-static-home-card
+            :data="data.bilingualList" 
+            :list="'bilingualList'"
+            class="mb-5"/>
+        </article>
+        <article v-if="data.citationList">
+          <h3 class="font-weight-regular home-title mb-2">Lista de voluntarios</h3>
+          <h4 class="font-weight-regular home-subheading mb-4">Número de personas en el listado de voluntarios</h4>
+          <stat-static-home-card
+            :data="data.citationList" 
+            :list="'citationList'"
+            class="mb-5"/>
+        </article>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
 </template>
 
 <script>
-import StatHomeCard from '@/components/StatHomeCard'
+import StatStaticHomeCard from '@/components/StatStaticHomeCard'
+import StatDynamicHomeCard from '@/components/StatDynamicHomeCard'
 export default {
   name: 'Home',
   components: {
-    StatHomeCard,
+    StatStaticHomeCard,
+    StatDynamicHomeCard,
   },
   data() {
     return {}
