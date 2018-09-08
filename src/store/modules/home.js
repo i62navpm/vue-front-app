6992ac5c03727a4d3c7a0eae24c6d2d5cf7effea
m.navarro <m.navarro@payvision.com>
2018-09-08 12:18:05 +0200

    Home component

--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -1,12 +1,28 @@
+import { db } from '@/plugins/firestore'
+import statsUtils from '@/utils/stats'
+
+const stats = statsUtils(db)
+
 export default {
   state: {
-    data: {},
+    data: null,
   },
   mutations: {
     setHome: (state, data) => (state.data = data),
   },
   actions: {
-    setHome: ({ commit }, { data }) => commit('setHome', data),
+    setHome: ({ commit }, data) => {
+      commit('setHome', data)
+    },
+    setStats: async ({ commit }) => {
+      let data = await stats.getStats([
+        'assignmentList',
+        'bilingualList',
+        'normalList',
+        'voluntaryList',
+      ])
+      commit('setHome', data)
+    },
   },
   getters: {
     getHome: state => state.data,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,50 @@
 <template>
-  <v-container fluid>
+  <v-container >
     <v-slide-y-transition mode="out-in">
       <v-layout 
-        column 
+        row 
         align-center>
-        <img 
-          src="@/assets/logo.png" 
-          alt="Vuetify.js" 
-          class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
+        <section>
+          <h3 class="font-weight-regular home-title mb-2">Últimos cambios en listas</h3>
+          <h4 class="font-weight-regular home-subheading mb-4">Mantente informado de los cambios en todas las listas</h4>
+          <stat-home-card :data="data.normalList" />
+        </section>
       </v-layout>
     </v-slide-y-transition>
   </v-container>
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<script>
+import StatHomeCard from '@/components/StatHomeCard'
+export default {
+  name: 'Home',
+  components: {
+    StatHomeCard,
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    data() {
+      return this.$store.state.home.data || {}
+    },
+  },
+  beforeCreate() {
+    if (!this.$store.state.home.data) {
+      this.$store.dispatch('setStats')
+    }
+  },
+}
+</script>
+
 <style scoped>
-h1,
-h2 {
-  font-weight: normal;
+.home-title {
+  color: rgba(0, 0, 0, 0.87);
+  font-size: 18px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.home-subheading {
+  color: rgba(0, 0, 0, 0.54);
+  font-size: 16px;
+  line-height: 24px;
 }
 </style>
