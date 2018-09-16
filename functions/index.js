837f844d4f7d9fd597a6c9dcb2cb41350b77b1fd
m.navarro <m.navarro@payvision.com>
2018-09-16 19:59:58 +0200

    New send email google function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions')
 const searchPerson = require('./searchPerson')
+const sendEmailFeedback = require('./sendEmailFeedback')
 
 exports.searchPerson = functions.https.onCall(searchPerson)
+exports.sendEmailFeedback = functions.https.onCall(sendEmailFeedback)
