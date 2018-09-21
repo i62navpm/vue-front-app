4e960b6e4efa83b623aba68fc01ac4cf179b10d5
m.navarro <m.navarro@payvision.com>
2018-09-21 19:43:09 +0200

    Adding spinner first load

---
+++ src/components/TheSpinner.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="spinner-block">
+
+    <v-layout 
+      align-center 
+      justify-center 
+      row 
+      fill-height>
+      <v-flex 
+        xs12 
+        class="text-xs-center">
+
+        <google/>  
+      </v-flex>
+    </v-layout>
+  </div>
+</template>
+
+
+<script>
+import { Google } from 'vue-loading-spinner'
+
+export default {
+  name: 'TheSpinner',
+  components: {
+    Google,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.spinner-block {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 30em;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,38 @@
 <template>
   <v-container>
     <v-slide-y-transition mode="out-in">
-      <v-layout 
-        column 
-        align-let>
-        <article v-if="data.normalList">
-          <h3 class="font-weight-regular home-title mb-2">Listado ordinario definitivo</h3>
-          <h4 class="font-weight-regular home-subheading mb-4">Número de personas en el listado ordinario</h4>
-          <v-stat-static-home-card 
-            :data="data.normalList" 
-            :list="'normalList'"
-            class="mb-5"/>
-        </article>
-        <article v-if="data.bilingualList">
-          <h3 class="font-weight-regular home-title mb-2">Listado bilingüe definitivo</h3>
-          <h4 class="font-weight-regular home-subheading mb-4">Número de personas en el listado bilingüe</h4>
-          <v-stat-static-home-card
-            :data="data.bilingualList" 
-            :list="'bilingualList'"
-            class="mb-5"/>
-        </article>
-        <article v-if="data.voluntaryList">
-          <h3 class="font-weight-regular home-title mb-2">Listado de voluntarios definitivo</h3>
-          <h4 class="font-weight-regular home-subheading mb-4">Número de personas en el listado de voluntarios</h4>
-          <v-stat-static-home-card
-            :data="data.voluntaryList" 
-            :list="'voluntaryList'"
-            class="mb-5"/>
-        </article>
-      </v-layout>
+      <div class="home-relative">
+        <v-layout 
+          v-if="data.normalList"
+          column 
+          align-left>
+          <article v-if="data.normalList">
+            <h3 class="font-weight-regular home-title mb-2">Listado ordinario definitivo</h3>
+            <h4 class="font-weight-regular home-subheading mb-4">Número de personas en el listado ordinario</h4>
+            <v-stat-static-home-card 
+              :data="data.normalList" 
+              :list="'normalList'"
+              class="mb-5"/>
+          </article>
+          <article v-if="data.bilingualList">
+            <h3 class="font-weight-regular home-title mb-2">Listado bilingüe definitivo</h3>
+            <h4 class="font-weight-regular home-subheading mb-4">Número de personas en el listado bilingüe</h4>
+            <v-stat-static-home-card
+              :data="data.bilingualList" 
+              :list="'bilingualList'"
+              class="mb-5"/>
+          </article>
+          <article v-if="data.voluntaryList">
+            <h3 class="font-weight-regular home-title mb-2">Listado de voluntarios definitivo</h3>
+            <h4 class="font-weight-regular home-subheading mb-4">Número de personas en el listado de voluntarios</h4>
+            <v-stat-static-home-card
+              :data="data.voluntaryList" 
+              :list="'voluntaryList'"
+              class="mb-5"/>
+          </article>
+        </v-layout>
+        <the-spinner v-else/>
+      </div>
     </v-slide-y-transition>
   </v-container>
 </template>
@@ -36,11 +40,14 @@
 <script>
 import VStatStaticHomeCard from '@/components/VStatStaticHomeCard'
 import VStatDynamicHomeCard from '@/components/VStatDynamicHomeCard'
+import TheSpinner from '@/components/TheSpinner'
+
 export default {
   name: 'Home',
   components: {
     VStatStaticHomeCard,
     VStatDynamicHomeCard,
+    TheSpinner,
   },
   computed: {
     data() {
@@ -51,6 +58,10 @@ export default {
 </script>
 
 <style scoped>
+.home-relative {
+  position: relative;
+  min-height: 30em;
+}
 .home-title {
   color: rgba(0, 0, 0, 0.87);
   font-size: 18px;
