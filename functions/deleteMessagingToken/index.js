463585c749b8e7bf8dc916312a1808b3b52d5254
m.navarro <m.navarro@payvision.com>
2018-09-23 11:27:19 +0200

    New delete messaging token function

--- functions/deleteMessagingToken/index.js
+++ functions/deleteMessagingToken/index.js
@@ -1,13 +1,13 @@
 const functions = require('firebase-functions')
 const axios = require('axios')
 const instance = axios.create({
-  baseURL: 'https://iid.googleapis.com/iid/v1',
+  baseURL: 'https://iid.googleapis.com/v1/web/iid',
 })
 instance.defaults.headers.common[
   'Authorization'
 ] = functions.config().server.key
 
 module.exports = (data, context) => {
-  instance.post(`/${data}/rel/topics/maestros`)
+  instance.delete(`/${data}`)
   return true
 }
