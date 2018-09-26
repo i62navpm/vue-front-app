6a81b143fe568e71f7c2587b054863f1e1e8de03
m.navarro <m.navarro@payvision.com>
2018-09-26 14:56:55 +0200

    Format file accord prettier

--- src/components/TheDialogLogin.vue
+++ src/components/TheDialogLogin.vue
@@ -128,7 +128,7 @@ export default {
           email,
           phoneNumber,
           photoURL,
-          createdAt: new Date().toISOString()
+          createdAt: new Date().toISOString(),
         })
       }
       this.$store.dispatch('setAuth', {
