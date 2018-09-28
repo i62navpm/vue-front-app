3c29af7bed3f700ed329ce1b4ded7e2dc4c6d0ea
m.navarro <m.navarro@payvision.com>
2018-09-28 15:05:55 +0200

    Toggle notifications and remove user functionality

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app'
+import { db } from '@/plugins/firestore'
+import { fb } from '@/plugins/firebaseFunctions'
 
 export default {
   state: {
@@ -14,13 +16,74 @@ export default {
       state.messaging = messaging
     },
     setLoading: (state, loading) => (state.loading = loading),
+    setEmailNotifications: (state, value = false) =>
+      (state.user.emailNotifications = value),
+    setPushNotifications: (state, value = false) =>
+      (state.user.pushNotifications = value),
   },
   actions: {
     setAuth: ({ commit }, auth) => commit('setAuth', auth),
+    toggleEmailNotifications: async ({ commit, state }, value) => {
+      try {
+        let querySnapshot = await db
+          .collection('users')
+          .where('email', '==', state.user.email)
+          .get()
+        querySnapshot.forEach(async doc => {
+          await doc.ref.update({
+            emailNotifications: value,
+          })
+        })
+        commit('setEmailNotifications', value)
+        return true
+      } catch (err) {
+        return err
+      }
+    },
+    togglePushNotifications: async ({ commit, state }, value) => {
+      try {
+        let querySnapshot = await db
+          .collection('users')
+          .where('email', '==', state.user.email)
+          .get()
+        querySnapshot.forEach(async doc => {
+          await doc.ref.update({
+            pushNotifications: value,
+          })
+        })
+
+        let googleFn = value
+          ? fb.httpsCallable('setMessagingToken')
+          : fb.httpsCallable('deleteMessagingToken')
+
+        await googleFn(state.messaging)
+
+        commit('setPushNotifications', value)
+        return true
+      } catch (err) {
+        throw new Error(err)
+      }
+    },
     logout: async ({ commit }) => {
       await firebase.auth().signOut()
       commit('setAuth', { user: {} })
       commit('setMessaging', { messaging: null })
+    },
+    remove: async ({ dispatch, state }) => {
+      try {
+        let user = firebase.auth().currentUser
+        let querySnapshot = await db
+          .collection('users')
+          .where('email', '==', state.user.email)
+          .get()
+
+        await fb.httpsCallable('deleteMessagingToken')(state.messaging)
+        await querySnapshot.forEach(doc => doc.ref.delete())
+        await fb.httpsCallable('removeUser')({ uid: user.uid })
+        return dispatch('logout')
+      } catch (err) {
+        throw new Error(err)
+      }
     },
   },
   getters: {
