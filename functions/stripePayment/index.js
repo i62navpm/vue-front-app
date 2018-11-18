b035d32562be043da15a162b30ae3d46cdde2cbc
m.navarro <m.navarro@payvision.com>
2018-11-18 19:41:03 +0100

    Added expired timestamp in milliseconds

--- functions/stripePayment/index.js
+++ functions/stripePayment/index.js
@@ -1,13 +1,19 @@
 const functions = require('firebase-functions')
+const { DateTime } = require('luxon')
 const tkStripe = functions.config().stripe.token
 const stripe = require('stripe')(tkStripe)
 const paymentType = require('./config')
+const months = { monthly: 1, termly: 3 }
 
 module.exports = (data, context) => {
   const charge = stripe.charges.create({
     ...paymentType[data.paymentType],
     source: data.token.id,
+    receipt_email: data.token.email,
   })
+  const expired = DateTime.fromMillis(data.token.created * 1000).plus({
+    months: months[data.paymentType],
+  }).ts
 
-  return charge
+  return { charge: charge.data, expired }
 }
