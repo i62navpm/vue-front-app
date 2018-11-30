9ff408192777de9b00c4f43b19225fc6a8f818ed
m.navarro <m.navarro@payvision.com>
2018-11-30 20:12:20 +0100

    Get payment status in home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,62 +2,70 @@
   <v-container>
     <v-slide-y-transition mode="out-in">
       <div class="home-relative">
-        <v-layout 
+        <v-layout
           v-if="data.normalList"
-          column 
-          align-left>
+          column
+          align-left
+        >
           <article v-if="data.normalList">
             <h3 class="font-weight-regular home-title mb-2">
               Listado definitivo de carácter ordinario
-              <v-download-button-list 
-                class="float-right" 
-                list="normalList"/>
+              <v-download-button-list
+                class="float-right"
+                list="normalList"
+              />
             </h3>
             <h4 class="font-weight-regular home-subheading mb-4">Distribución de personas por especialidades</h4>
-            <v-stat-static-home-card 
-              :data="data.normalList" 
+            <v-stat-static-home-card
+              :data="data.normalList"
               :list="'normalList'"
-              class="mb-5"/>
+              class="mb-5"
+            />
           </article>
           <article v-if="data.bilingualList">
             <h3 class="font-weight-regular home-title mb-2">
               Listado definitivo de especialidades bilingües
-              <v-download-button-list 
-                class="float-right" 
-                list="bilingualList"/>
+              <v-download-button-list
+                class="float-right"
+                list="bilingualList"
+              />
             </h3>
             <h4 class="font-weight-regular home-subheading mb-4">Distribución de personas por especialidades</h4>
             <v-stat-static-home-card
-              :data="data.bilingualList" 
+              :data="data.bilingualList"
               :list="'bilingualList'"
-              class="mb-5"/>
+              class="mb-5"
+            />
           </article>
           <article v-if="data.voluntaryList">
             <h3 class="font-weight-regular home-title mb-2">
               Listado definitivo de carácter voluntario
-              <v-download-button-list 
-                class="float-right" 
-                list="voluntaryList"/>
+              <v-download-button-list
+                class="float-right"
+                list="voluntaryList"
+              />
             </h3>
             <h4 class="font-weight-regular home-subheading mb-4">Distribución de personas por especialidades</h4>
             <v-stat-static-home-card
-              :data="data.voluntaryList" 
+              :data="data.voluntaryList"
               :list="'voluntaryList'"
-              class="mb-5"/>
+              class="mb-5"
+            />
           </article>
 
-          <article>
-            <the-pricing/>
+          <article v-if="!hasPaid">
+            <the-pricing />
           </article>
-            
+
         </v-layout>
-        <the-spinner v-else/>
+        <the-spinner v-else />
       </div>
     </v-slide-y-transition>
   </v-container>
 </template>
 
 <script>
+import { fb } from '@/plugins/firebaseFunctions'
 import VStatStaticHomeCard from '@/components/VStatStaticHomeCard'
 import VStatDynamicHomeCard from '@/components/VStatDynamicHomeCard'
 import VDownloadButtonList from '@/components/VDownloadButtonList'
@@ -73,10 +81,22 @@ export default {
     TheSpinner,
     ThePricing,
   },
+  data: () => {
+    return {
+      hasPaid: false,
+    }
+  },
   computed: {
     data() {
       return this.$store.state.home.data || {}
     },
+  },
+  async created() {
+    const { data } = await fb.httpsCallable('hasPaid')({
+      email: this.$store.getters.getAuth.email,
+      time: new Date().getTime(),
+    })
+    this.hasPaid = !!data
   },
 }
 </script>
