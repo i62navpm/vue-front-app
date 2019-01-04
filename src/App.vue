2b69f0ac90bf0580043777da8cde8b664a171551
m.navarro <m.navarro@payvision.com>
2019-01-04 11:23:09 +0100

    Save snackbar for further notifications

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,45 @@
 <template>
   <v-app>
-
+    <v-snackbar
+      v-model="snackbar"
+      :timeout="20000"
+      vertical
+      right
+      auto-height
+      top
+    >
+      <p class="headline yellow--text">
+        <v-icon
+          color="yellow"
+          class="mr-2"
+        >warning</v-icon>
+        Noticia
+      </p>
+      <span>
+        A partir del 1 de Enero de 2019 Listando<span class="orange--text">Me</span> empezará a usar un modelo de suscripción.
+      </span>
+      <span class="mt-2 mb-5">
+        Hemos agotado el presupuesto del que disponíamos y nos hemos visto forzados a usar este modelo, era esto o cerrar Listando<span class="orange--text">Me</span>.
+      </span>
+      <span class="text-xs-right">
+        Sentimos mucho todas las molestias 😔😔😔
+      </span>
+      <v-btn
+        color="info"
+        flat
+        class="text-xs-left"
+        @click="goToPrizes()"
+      >
+        Ver suscripción
+      </v-btn>
+      <v-btn
+        color="pink"
+        flat
+        @click="snackbar = false"
+      >
+        Cerrar
+      </v-btn>
+    </v-snackbar>
     <the-toolbar />
     <the-sidebar />
 
@@ -15,6 +54,7 @@
 </template>
 
 <script>
+import VueScrollTo from 'vue-scrollto'
 import TheToolbar from '@/components/TheToolbar'
 import TheFooter from '@/components/TheFooter'
 import TheSidebar from '@/components/TheSidebar'
@@ -30,8 +70,28 @@ export default {
     TheFooter,
     TheFabButton,
   },
+  data() {
+    return {
+      snackbar: false,
+    }
+  },
+  mounted() {
+    this.snackbar = false
+  },
   async beforeMount() {
     await this.$store.dispatch('checkPaid')
+  },
+  methods: {
+    goToPrizes() {
+      this.snackbar = false
+      this.$router.push({ name: 'home' })
+      setTimeout(() => {
+        VueScrollTo.scrollTo('#pricing', 200, {
+          easing: 'ease-in',
+          offset: -125,
+        })
+      })
+    },
   },
 }
 </script>
