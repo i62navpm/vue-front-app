a746bf16bf50d9162c33a1882ef857a79fb9614a
m.navarro <m.navarro@payvision.com>
2019-08-31 20:10:50 +0200

    chore: change sender id

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,7 @@
 importScripts('https://www.gstatic.com/firebasejs/5.4.2/firebase-app.js')
 importScripts('https://www.gstatic.com/firebasejs/5.4.2/firebase-messaging.js')
 
-firebase.initializeApp({ messagingSenderId: '635428808415' })
+firebase.initializeApp({ messagingSenderId: '102684721042' })
 
 const messaging = firebase.messaging()
 
