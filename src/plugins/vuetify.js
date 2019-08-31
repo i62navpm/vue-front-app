ad34079b273c07992e3a5f705701a20e57ecdf50
i62navpm <manuelnavarro1987@gmail.com>
2019-08-31 14:59:37 +0200

    feat: added vuetify

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib'
+import es from 'vuetify/es5/locale/es'
+
+Vue.use(Vuetify)
+
+export default new Vuetify({
+  lang: {
+    locales: { es },
+    current: 'es',
+  },
+  icons: {
+    iconfont: 'md',
+  },
+})
