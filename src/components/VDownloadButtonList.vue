8db4acb0cf8336d4c95d2421c89797d76e4941ae
m.navarro <m.navarro@payvision.com>
2019-09-01 18:08:07 +0200

    fix: fix download files

--- src/components/VDownloadButtonList.vue
+++ src/components/VDownloadButtonList.vue
@@ -109,7 +109,6 @@ export default {
         const { data = [] } = await this.getListFiles(this.list)
         this.availableFiles = data
         this.date = this.availableFiles[this.availableFiles.length - 1]
-        this.url = await this.getFileUrl(`${this.list}/${this.date}.pdf`)
       } catch (err) {
         captureException(err)
       } finally {
