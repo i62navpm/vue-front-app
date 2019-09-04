993894c25dccfe63918f8c65cb07e57872b99f77
m.navarro <m.navarro@payvision.com>
2019-09-04 21:48:58 +0200

    feat: added date filter

--- functions/queriesBigQuery/index.js
+++ functions/queriesBigQuery/index.js
@@ -23,7 +23,7 @@ async function existsDateColumn(data = {}) {
 exports.getDatesList = async (data = {}) => {
   const { datasetId, tableId } = data
 
-  const query = `SELECT DISTINCT(date) FROM ${datasetId}.${tableId} WHERE date <= CURRENT_DATE()`
+  const query = `SELECT DISTINCT(date) FROM ${datasetId}.${tableId} WHERE date <= CURRENT_DATE() order by date`
 
   const options = {
     query,
@@ -38,14 +38,16 @@ exports.getPaginateList = async (data = {}) => {
     tableId,
     config: {
       paginate = { page: 0, limit: 10 },
-      filter = { name: '' },
+      filter = { name: '', date: '' },
       sort = 'orden',
       order = 'ASC',
     },
   } = data
 
   const query = `SELECT * FROM ${datasetId}.${tableId} 
-  ${filter.name ? `WHERE apellidosynombre LIKE "${filter.name}%"` : ''} 
+  WHERE apellidosynombre LIKE "${filter.name}%" ${
+    filter.date ? `AND date = "${filter.date}"` : ''
+  } 
   ORDER BY ${sort} ${order} LIMIT @limit OFFSET @page`
 
   const options = {
