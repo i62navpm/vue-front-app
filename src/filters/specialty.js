1ef544c8107eb127347a6a643a0c36cd1ccafffd
m.navarro <m.navarro@payvision.com>
2019-09-04 20:17:56 +0200

    feat: added id and description list filter

--- src/filters/specialty.js
+++ src/filters/specialty.js
@@ -1,21 +1,21 @@
+import { specialtyCodes } from '@i62navpm/specialty-codes'
+let mapSpecialty = specialtyCodes.primary.normalSpecialties.reduce(
+  (acc, curr) => {
+    acc[curr.id] = curr.description
+    return acc
+  },
+  {}
+)
+mapSpecialty = specialtyCodes.primary.bilingualSpecialties.reduce(
+  (acc, curr) => {
+    acc[curr.id] = curr.description
+    return acc
+  },
+  mapSpecialty
+)
+
 export default function(value) {
   if (!value) return ''
 
-  const mapSpecialty = {
-    'al-audicionylenguaje': '[AL] Audición y lenguaje',
-    as: '[AS] Audición y lenguaje',
-    'ef-educacionfisica': '[EF] Educación física',
-    'ei-educacioninfantil': '[EI] Educación infantil',
-    'fi-lenguaextranjera:ingles': '[FI] Lengua extranjera inglés',
-    'mu-musica': '[MU] Música',
-    'pri-primaria': '[PRI] Primaria',
-    ps: '[PS] Pedagogía terapéutica',
-    'pt-pedagogiaterapeutica': '[PT] Pedagogía terapéutica',
-    '001-primaria_ingles': '[001] Primaria / inglés',
-    '002-educacioninfantil_ingles': '[002] Educación infantil / inglés',
-    '003-musica_ingles': '[003] Música / inglés',
-    '004-educacionfisica_ingles': '[004] Educación física / inglés',
-    '005-ingles_ingles': '[005] Inglés / inglés',
-  }
   return mapSpecialty[value] || value
 }
