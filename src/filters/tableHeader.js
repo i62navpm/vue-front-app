a7ff71e371dcdcad7bed36265806e97c37025aa0
m.navarro <m.navarro@payvision.com>
2019-09-04 21:49:09 +0200

    feat: added literal translations

--- src/filters/tableHeader.js
+++ src/filters/tableHeader.js
@@ -4,11 +4,22 @@ export default function(value) {
   const mapSpecialty = {
     apellidosynombre: 'Apellidos y nombre',
     tipodevacantes: 'Tipo de vacantes',
-    'centroasignado/funciónasignada': 'Centro asignado/Funcón asignada',
+    'centroasignado/funciónasignada': 'Centro asignado/Función asignada',
     datpresentación: 'Dat. presentación',
     tipopuesto: 'Tipo de puesto',
+    tipovacante: 'Tipo de vacante',
     listName: 'Listado',
-    ptos: 'Puntuación',
+    puntuacion: 'Puntuación',
+    date: 'Fecha',
+    asinacion: 'Asignación',
+    nif: 'NIF',
+    baremo11: 'Baremo 1.1',
+    baremo12: 'Baremo 1.2',
+    baremo13: 'Baremo 1.3',
+    baremo14: 'Baremo 1.4',
+    baremo211: 'Baremo 2.1.1',
+    baremo212: 'Baremo 2.1.2',
+    baremo22: 'Baremo 2.2',
   }
   return mapSpecialty[value] || value
 }
